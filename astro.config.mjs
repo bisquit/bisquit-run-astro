3cbcadc2dbd2eba7f129b9f2a559ca1ff1cd9d98
bisquit <qu1dpr09u0@gmail.com>
2022-08-18 20:32:07 +0900

    build for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify/functions';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind()],
   output: 'server',
-  adapter: netlify(),
+  adapter: cloudflare(),
 });
