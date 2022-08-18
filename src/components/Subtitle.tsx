06bca53fb47536ca42e1fdf7af0047084ec6e3fa
bisquit <qu1dpr09u0@gmail.com>
2022-08-18 15:20:55 +0900

    fix: add link

--- src/components/Subtitle.tsx
+++ src/components/Subtitle.tsx
@@ -1,8 +1,16 @@
 import type { ComponentChildren } from 'preact';
 
-function Subtitle(props: { children?: ComponentChildren }) {
-  const { children } = props;
-  return <h2 className="mb-12 text-2xl font-bold">{children}</h2>;
+function Subtitle(props: { sectionId?: string; children?: ComponentChildren }) {
+  const { sectionId, children } = props;
+  return sectionId ? (
+    <a href={'#' + sectionId}>
+      <h2 id={sectionId} className="mb-8 py-4 text-2xl font-bold">
+        {children}
+      </h2>
+    </a>
+  ) : (
+    <h2 className="mb-8 py-4 text-2xl font-bold">{children}</h2>
+  );
 }
 
 export default Subtitle;
